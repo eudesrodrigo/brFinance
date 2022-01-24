05e2bd19e8f30dfcde758aaa882b1895ce44a0c5
Eudes Oliveira <eudesrodrigo@outlook.com>
2022-01-24 14:25:27 -0300

    Fixes

--- setup.py
+++ setup.py
@@ -19,7 +19,11 @@ install_requires = [
 
 setuptools.setup(
     name='brfinance',
+<<<<<<< HEAD
     version='0.0.8',
+=======
+    version='0.0.6',
+>>>>>>> parent of e6d1504 (Corrigido erro de letra maiuscula no nome da lib)
     packages=setuptools.find_packages(),
     python_requires='>=3.7.*',
     author='Eudes Rodrigo Nunes de Oliveira',
